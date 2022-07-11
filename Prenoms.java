aa26c6dca06f61da0a60b8a51a1134b9bfbef313
Kimonae <70099141+Kimonae@users.noreply.github.com>
2022-07-11 14:59:46 +0200

    Little fail

--- Prenoms.java
+++ Prenoms.java
@@ -9,12 +9,12 @@ public class Prenoms {
 		System.out.println("Entrez votre Prénom, Nom");
 		Scanner sc= new Scanner(System.in);
 		String prenom =sc.nextLine();
-		Scanner sc2= new Scanner(System.in);
+	
 		String nom =sc.nextLine(); 
 		
 		System.out.println("Saisissez un Age"); 
 		int age = sc.nextInt();
-		Scanner sc3= new Scanner(System.in);
+	
 		System.out.println(prenom + nom + age); 
 	}
 
